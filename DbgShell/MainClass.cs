microsoft/DbgShell

commit 07485ef29c35256dcee193151f3ad8f49c8c9b33

Set Console.OutputEncoding to Unicode.
This allows "[Console]::WriteLine( [char] 0x2026 )" to work just as well
as "[char] 0x2026".

DbgShell/MainClass.cs
@@ -54,6 +54,8 @@ namespace MS.DbgShell
 
         static int Main( string[] args )
         {
+            Console.OutputEncoding = Encoding.Unicode;
+
             //
             // We've got four possibilities:
             //

commit 7cf3747681eeb6ac320e0e638b753c18612edfcb

Fix regression: console output encoding caused garbled output
In commit 07485ef29c, I added this as the first line of Main:

    Console.OutputEncoding = Encoding.Unicode;

With the comment:

    This allows "[Console]::WriteLine( [char] 0x2026 )" to work just as
    well as "[char] 0x2026".

But this caused output from other programs (that weren't emitting UTF16)
to be garbled (because the output was interpreted as UTF16 when captured
into a variable) (#95).

Hopefully setting the output encoding to UTF8 will work out better. At
least it lets both "[Console]::WriteLine( [char] 0x2026 )" and "git
--version" to work.

Addresses #95.

DbgShell/MainClass.cs
@@ -54,7 +54,9 @@ namespace MS.DbgShell
 
         static int Main( string[] args )
         {
-            Console.OutputEncoding = Encoding.Unicode;
+            // This allows "[Console]::WriteLine( [char] 0x2026 )" to work just as well as
+            // "[char] 0x2026".
+            Console.OutputEncoding = Encoding.UTF8;
 
             //
             // We've got four possibilities: